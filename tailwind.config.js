f18a59e7e0630cae40481be11526b88cd469049e
JY8752 <j.y.8752@gmail.com>
2022-05-15 17:50:59 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {},
+  mode: 'jit',
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
